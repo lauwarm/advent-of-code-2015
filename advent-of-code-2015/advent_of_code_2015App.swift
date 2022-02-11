38f593ee79eceffe297225df74750f6eab0aed29
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-02-11 11:14:18 +0100

    Initial Commit

---
+++ advent-of-code-2015/advent_of_code_2015App.swift
@@ -0,0 +1,17 @@
+//
+//  advent_of_code_2015App.swift
+//  advent-of-code-2015
+//
+//  Created by Fabian on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct advent_of_code_2015App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ advent-of-code-2015UITests/advent_of_code_2015UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  advent_of_code_2015UITestsLaunchTests.swift
+//  advent-of-code-2015UITests
+//
+//  Created by Fabian on 11.02.22.
+//
+
+import XCTest
+
+class advent_of_code_2015UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
